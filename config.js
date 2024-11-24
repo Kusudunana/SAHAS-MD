0718833814248504d140dcb43b7390649c1bd9ee
Kusudunana <Xuejenicid@gmail.com>
2024-11-24 14:09:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "Put Session Id",
+  SESSION_ID: process.env.SESSION_ID || "9Sl22T4Z#RANn-mLbR_1GlaFxCWEaS4bsnb9shZMCkJV7MxNdUYg",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
